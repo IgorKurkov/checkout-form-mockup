5c5be6fc75d8d52b928ddaccc480ea228135a6a5
Igor Kurkov <kurkov87@gmail.com>
2018-01-22 01:51:24 +0200

    header finished

--- gulpfile.js
+++ gulpfile.js
@@ -24,8 +24,8 @@ gulp.task("reload", () => {
 });
 
 gulp.task('less', () => {
-  return gulp.src('app/less/**/*.less')
-  // return gulp.src('app/less/styles.less')
+  // return gulp.src('app/less/**/*.less')
+  return gulp.src('app/less/styles.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }).on("error", notify.onError()))
